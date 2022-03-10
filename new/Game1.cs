65f4b2dd523f40f1b357f6436e586b5ce00d298d
dan01010101 <98850770+dan01010101@users.noreply.github.com>
2022-03-10 17:03:53 +0000

    Add files via upload

---
+++ new/Game1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Nightmare
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        //SpriteBatch spriteBatch;
+
+        GamePlay gamePlay;
+
+        Images cursor;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            Globals.screenWidth = 1600; //1600
+            Globals.screenHeight = 900; //900
+
+            graphics.PreferredBackBufferWidth = Globals.screenWidth;
+            graphics.PreferredBackBufferHeight = Globals.screenHeight;
+
+            graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            Globals.content = this.Content;
+            Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            cursor = new Images("Reticle", new Vector2(0, 0), new Vector2(7, 7));
+
+            Globals.keyboard = new TheKeyboard();
+            Globals.mouse = new TheMouse();
+
+            gamePlay = new GamePlay();
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Globals.gameTime = gameTime;
+            Globals.keyboard.Update();
+            Globals.mouse.Update();
+
+
+            gamePlay.Update();
+
+
+            Globals.keyboard.UpdateOld();
+            Globals.mouse.UpdateOld();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            Globals.spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+
+
+            gamePlay.Draw();
+
+
+            cursor.Draw(new Vector2(Globals.mouse.newMousePos.X, Globals.mouse.newMousePos.Y), new Vector2(0, 0), Color.White);
+            Globals.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
