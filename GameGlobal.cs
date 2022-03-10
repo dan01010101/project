ea837c4103229e78eaced4c629e63439daabe211
dan01010101 <98850770+dan01010101@users.noreply.github.com>
2022-03-10 16:41:26 +0000

    Add files via upload

---
+++ GameGlobal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Nightmare
+{
+    public class GameGlobal
+    {
+        public static int score = 0;
+
+        public static PassObject PassProjectile, PassEnemy, PassSpawnPoint, CheckScroll;
+    }
+}
